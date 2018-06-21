c317133f66a8e07317e6621f177439a0722e4c58
xuyangyang <376892900@qq.com>
2018-06-21 08:56:27 +0800

    首次提交F版项目目录，提交eureka,config模块

---
+++ finchley-config/src/main/java/com/madecare/springcloud/finchley/config/FinchleyConfigApplication.java
@@ -0,0 +1,14 @@
+package com.madecare.springcloud.finchley.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class FinchleyConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinchleyConfigApplication.class, args);
+    }
+}
---
+++ finchley-eureka/src/main/java/com/madecare/springcloud/finchley/eureka/FinchleyEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.madecare.springcloud.finchley.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinchleyEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinchleyEurekaApplication.class, args);
+    }
+}
