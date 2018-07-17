5e67e40c98f683c0c8c2b59ed02048a50034bb35
xuyangyang <376892900@qq.com>
2018-07-17 16:00:39 +0800

    commit new functions

---
+++ finchley-admin/src/main/java/com/madecare/springcloud/finchley/admin/FinchleyAdminApplication.java
@@ -0,0 +1,16 @@
+package com.madecare.springcloud.finchley.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class FinchleyAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinchleyAdminApplication.class, args);
+    }
+}
